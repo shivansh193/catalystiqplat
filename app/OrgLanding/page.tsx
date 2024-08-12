578468b697fb7612bb6760591b89daf5a805c126
shivansh193 <shivanshkalra796@gmail.com>
2024-08-13 02:43:16 +0530

    fixes

---
+++ app/OrgLanding/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+import LLnding from "@/components/landing";
+import React from "react"
+import "./pp.css"
+
+export default function Home() {
+
+    return(
+        <>
+        <LLnding />
+        </>
+    );
+}
